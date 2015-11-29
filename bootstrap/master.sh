531549c27b7ce50adec12f269a48a1df376dea75
Morgan <morgan@salt.infra.macapinlac.com>
2015-11-29 13:10:59 -0800

    Updated master bootstrap script with proper wget parameters

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -2,8 +2,7 @@ sudo add-apt-repository -y ppa:saltstack/salt
 sudo apt-get update
 sudo apt-get install -y python-pip salt-master salt-minion python-gnupg git-core
 
-sudo rm /etc/salt/master
-sudo wget -O - https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/prod/master > /etc/salt/master
+sudo wget -O /etc/salt/master https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/prod/master
 
 cd /srv
 sudo git clone https://github.com/rsmacapinlac/hwhl hwhl-prod
