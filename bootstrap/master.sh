fa03ef61ada2bc3e0ef669034e5323615e494704
Morgan <morgan@salt.infra.macapinlac.com>
2015-11-29 13:05:52 -0800

    Modified master bootstrap script to allow itself to be called remotely

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -2,7 +2,8 @@ sudo add-apt-repository -y ppa:saltstack/salt
 sudo apt-get update
 sudo apt-get install -y python-pip salt-master salt-minion python-gnupg git-core
 
-sudo cp ../configs/prod/master /etc/salt/master
+sudo rm /etc/salt/master
+sudo wget -O - https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/prod/master > /etc/salt/master
 
 cd /srv
 sudo git clone https://github.com/rsmacapinlac/hwhl hwhl-prod
