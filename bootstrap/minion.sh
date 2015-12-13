e645660a36e21b2726d083911003ded6ea7cc7dc
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2015-12-12 20:28:25 -0800

    Updated minion script and minion bootstrap configuration, updated master autosign and autoreject

--- bootstrap/minion.sh
+++ bootstrap/minion.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository -y ppa:saltstack/salt
 sudo apt-get update
 sudo apt-get install -y git-core python-pip salt-minion python-gnupg
 
-sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/shared/minion
+sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/bootstrap/minion
 sudo sh -c 'echo shino.db.macapinlac.com > /etc/hostname'; sudo hostname shino.db.macapinlac.com
 
 sudo service salt-minion restart
--- salt/states/saltstack/config.sls
+++ salt/states/saltstack/config.sls
@@ -4,3 +4,16 @@ git-core:
 /etc/salt/master:
   file.managed:
     - source: salt://saltstack/files/master
+
+/etc/salt/autosign.conf:
+  file.managed:
+    - source: salt://saltstack/files/autosign.conf
+
+/etc/salt/autoreject.conf:
+  file.managed:
+    - source: salt://saltstack/files/autoreject.conf
+
+salt-master:
+  service.running:
+    - enable: True
+    - reload: True
