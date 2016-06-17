3f87202e5bfa2f62859fb47bee621ec04fdbb9ef
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2016-06-16 19:53:50 -0700

    Updated the minion script

--- bootstrap/minion.sh
+++ bootstrap/minion.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# setup the hostname from the first param
+sudo sh -c 'echo $1 > /etc/hostname'; sudo hostname $1
+
 # get and install the saltstack
 sudo add-apt-repository -y ppa:saltstack/salt
 sudo apt-get update
 sudo apt-get install -y python-pip salt-minion python-gnupg
 
-sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/configs/bootstrap/minion
-sudo sh -c 'echo shino.db.macapinlac.com > /etc/hostname'; sudo hostname shino.db.macapinlac.com
+# /srv/hwhl/salt/states/saltstack/files/minion /etc/salt/minion
+sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/salt/states/saltstack/files/minion
+sudo sed -i "s/{{ pillar\['network'\]\['saltmaster'\] }}/$2/g" /etc/salt/minion
 
 sudo service salt-minion restart
