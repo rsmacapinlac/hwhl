ff41b15256fca98a31ccfeb5b617e82e4381880d
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2016-06-18 10:37:46 -0700

    Updated minion bootstrapper

--- bootstrap/minion.sh
+++ bootstrap/minion.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# setup the hostname from the first param
-sudo sh -c 'echo $1 > /etc/hostname'; sudo hostname $1
+sudo apt-get install -y --allow-unauthenticated python-pip salt-minion
+python-gnupg
 
-# get and install the saltstack
-sudo add-apt-repository -y ppa:saltstack/salt
-sudo apt-get update
-sudo apt-get install -y python-pip salt-minion python-gnupg
-
-# /srv/hwhl/salt/states/saltstack/files/minion /etc/salt/minion
 sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/salt/states/saltstack/files/minion
-sudo sed -i "s/{{ pillar\['network'\]\['saltmaster'\] }}/$2/g" /etc/salt/minion
+
+# copy the minion configuration from the repo
+# sudo cp /srv/hwhl/salt/states/saltstack/files/minion /etc/salt/minion
+sudo sed -i "s/{{ pillar\['network'\]\['saltmaster'\] }}/$1/g" /etc/salt/minion
+# sudo cat /etc/salt/minion
 
 sudo service salt-minion restart
