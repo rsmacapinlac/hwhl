d94118f0e9e5b9507bf4102ba5ac546fe0e04103
root <root@makarov.infra.macapinlac.com>
2016-06-17 21:29:16 -0700

    Updated network script

--- salt/states/network/files/build_network.py
+++ salt/states/network/files/build_network.py
@@ -17,9 +17,9 @@ _active_minions = r.cmd("manage.up")
 for minion in _active_minions:
   _g = c.cmd(minion, 'grains.items')
 
-  network_data['network']['hosts'][_g[minion]['host']]['ipaddr'] = _g[minion]['ip_interfaces']['eth1'][0]
+  network_data['network']['hosts'][_g[minion]['host']]['ipaddr'] = _g[minion]['ipv4'][1]
   #print network_data['network']['hosts']
-  #print minion + ': ' + _g[minion]['ip_interfaces']['eth0'][0]
+  #print minion + ': ' + _g[minion]['ipv4'][1]
 
 _network_data = open(network_datafile, 'w')
 _network_data.write(yaml.dump(network_data, default_flow_style=False, allow_unicode=True))
