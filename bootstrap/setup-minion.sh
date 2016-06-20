356039f05b747087a43d082417a7391b93cd8484
Ritchie Macapinlac <rsmacapinlac@boogienet.com>
2016-06-19 17:13:57 -0700

    Modified setup-minion to pull from github (instead of assume the repo is local)

--- bootstrap/setup-minion.sh
+++ bootstrap/setup-minion.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y python-pip salt-minion python-gnupg
+sudo apt-get -y install salt-minion
 
 # copy the minion configuration from the repo
-sudo cp /srv/hwhl/salt/states/saltstack/files/minion /etc/salt/minion
+sudo wget -O /etc/salt/minion https://raw.githubusercontent.com/rsmacapinlac/hwhl/master/salt/states/saltstack/files/minion
+# sudo cp /srv/hwhl/salt/states/saltstack/files/minion /etc/salt/minion
 sudo sed -i "s/{{ pillar\['network'\]\['saltmaster'\] }}/$1/g" /etc/salt/minion
-sudo cat /etc/salt/minion
+# sudo cat /etc/salt/minion
 
 sudo service salt-minion restart
